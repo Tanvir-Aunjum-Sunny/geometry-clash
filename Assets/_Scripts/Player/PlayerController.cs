b48cd7f850a7e163327dbffedee4ba5269ac03e4
Kendall Roth <kendall@kendallroth.ca>
2018-12-15 22:57:35 -0500

    Fix editor bug in Player Controller
    The Player Controller accessed a singleton instance (game manager) when drawing gizmos. However, this doesn't work while the game is not running and caused a lot of (annoying) errors.

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -45,6 +45,8 @@ public class PlayerController : ExtendedMonoBehaviour
 
     private void OnDrawGizmos()
     {
+        if (GameManager.Instance == null || !GameManager.Instance.DebugMode) return;
+
         // Show mouse position
         Gizmos.color = new Color(239, 193, 186);
         Gizmos.DrawSphere(mousePoint, 0.15f);
